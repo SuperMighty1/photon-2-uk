85d21d46e38fd879946a5c415747f061aacb2cd1
SuperMighty1 <supermighty1@gmail.com>
2025-02-15 12:58:22 +0000

    '02 vectra (beta??)

---
+++ lua/autorun/sm02vectra.lua
@@ -0,0 +1,17 @@
+local V = {
+	-- Required information
+	Name = "2002 Opel Vectra",
+	Model = "models/smcars/2002_vectra.mdl",
+	Class = "prop_vehicle_jeep",
+	Category = "Super's Cars",
+
+	-- Optional information
+	Author = "Super Mighty",
+	Information = "pon pon wei wei wei",
+
+	KeyValues = {
+
+		vehiclescript = "scripts/vehicles/superscars/13_bmwf11.txt"
+	}
+}
+list.Set( "Vehicles", "sm02vectra", V )
